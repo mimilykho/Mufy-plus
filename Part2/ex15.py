35d52e0815af52b7e6a17d4cebedfaa12ca00cc9
mimilykho <emilykhoenhee@gmail.com>
2026-05-26 04:42:06 +0000

    done

---
+++ Part2/ex15.py
@@ -0,0 +1,7 @@
+import random
+flowers=['rose','tulip','lily']
+flowers_random= random.choice(flowers)
+print(flowers_random)
+
+number= random.randint(1,100)
+print(number)
---
+++ Part2/ex16.py
@@ -0,0 +1,3 @@
+name = input('What is your name?\n')
+food = input('What is your favourite food?\n')
+print(f'Hi {name} ,I heard you love {food}!')
